65ff931fd9bd73cde833a52bdcd20093fdbfcf40
Marthin Korb <marthinkorb@gmail.com>
2020-10-22 00:09:55 -0300

    Adding Swagger

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import swaggerJsDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
 import cors from 'cors';
 import 'express-async-errors';
 
@@ -8,8 +10,33 @@ import errorHandler from './errors/handler';
 
 const app = express();
 
+const swaggerOptions = {
+    swaggerDefinition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'VUTTR API',
+            version: '1.0.0',
+        }
+    },
+    apis: ['./routes.ts'],
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+console.log(swaggerDocs);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
 app.use(cors());
 app.use(express.json());
+/**
+ * @swagger
+ *   /tools:
+ *      get:
+ *          description: List all tools
+ *      responses: 
+ *          200
+ *          description: Success
+ */
 app.use(routes);
 app.use(errorHandler);
 
